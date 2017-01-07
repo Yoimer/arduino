09fd33db9a745cd02ebf89970fe9b71e8991345e
Łukasz Budnik <lukasz.budnik@gmail.com>
2017-01-07 13:18:12 +0100

    more refactoring, preparations for bluetooth project

--- ir_robot_control/ir_robot_control.ino
+++ ir_robot_control/ir_robot_control.ino
@@ -1,111 +1,10 @@
 #include <IRremote.h>
 
 #include "robot.h"
-#include "list.h"
-#include "map.h"
+#include "robot_ir.h"
 
 #include "list.cpp"
 #include "map.cpp"
-
-/****************************************
- *                                      *
- * Moving Robot                         *
- *                                      *
- ****************************************/
-
-void move(unsigned char forward) {
-  if (forward) {
-    Serial.println("Moving forward");
-  } else {
-    Serial.println("Moving reverse");
-  }
-  digitalWrite(ROBOT_RIGHT_MOTOR_PIN1, forward);
-  digitalWrite(ROBOT_RIGHT_MOTOR_PIN2, !forward);
-
-  digitalWrite(ROBOT_LEFT_MOTOR_PIN1, forward);
-  digitalWrite(ROBOT_LEFT_MOTOR_PIN2, !forward);
-
-  delay(ROBOT_MOVE_LENGHT);  
-}
-
-void stop() {
-  digitalWrite(ROBOT_RIGHT_MOTOR_PIN1, LOW);
-  digitalWrite(ROBOT_RIGHT_MOTOR_PIN2, LOW);
-
-  digitalWrite(ROBOT_LEFT_MOTOR_PIN1, LOW);
-  digitalWrite(ROBOT_LEFT_MOTOR_PIN2, LOW);
-
-  delay(ROBOT_DELAY_LENGHT);
-}
-
-void turn(short deg) {
-  Serial.print("Turning by ");
-  Serial.println(deg);
-
-  unsigned char left = LOW;
-  unsigned char right = LOW;
-
-  // turning left:
-  // left motor reverses while right one moves forward
-  if (deg < 0) {
-    right = HIGH;
-    deg = -deg;
-  } else {
-    left = HIGH;
-  }
-
-  unsigned short delayMs = (deg / 90.0) * ROBOT_TURN_LENGHT;
-
-  Serial.print("Move will take ");
-  Serial.print(delayMs);
-  Serial.println(" ms");
-  
-  digitalWrite(ROBOT_LEFT_MOTOR_PIN1, left);
-  digitalWrite(ROBOT_LEFT_MOTOR_PIN2, !left);
-
-  digitalWrite(ROBOT_RIGHT_MOTOR_PIN1, right);
-  digitalWrite(ROBOT_RIGHT_MOTOR_PIN2, !right);
-
-  delay(delayMs);
-}
-
-void execute(unsigned char direction) {
-  switch (direction) {
-    case ROBOT_N:
-      move(HIGH);
-      break;
-    case ROBOT_S:
-      move(LOW);
-      break;
-    case ROBOT_W:
-      turn(-90);
-      break;
-    case ROBOT_NW:
-      turn(-45);
-      break;
-    case ROBOT_SW:
-      turn(-135);
-      break;
-    case ROBOT_E:
-      turn(90);
-      break;
-    case ROBOT_NE:
-      turn(45);
-      break;
-    case ROBOT_SE:
-      turn(135);
-      break;
-  }
-  stop();
-}
-
-void replay_moves(struct node<unsigned char>* list) {
-  struct node<unsigned char>* tmp = list;
-  while (tmp != NULL) {
-    execute(tmp->value);
-    tmp = tmp->next;
-  }
-}
 
 /****************************************
  *                                      *
@@ -114,11 +13,8 @@ void replay_moves(struct node<unsigned char>* list) {
  ****************************************/
 
 IRrecv irrecv(ROBOT_IR_PIN);
-// key = direction = unsigned char
-// value = code = long
+Robot robot;
 struct item<unsigned char, unsigned long>* codes;
-struct node<unsigned char>* directions;
-bool programming = 0;
 
 /****************************************
  *                                      *
@@ -160,30 +56,145 @@ void loop() {
   if (irrecv.decode(&results)) {
     irrecv.resume();
     if (results.value == ROBOT_IR_REMOTE_FUNC) {
-      programming = !programming;
-      if (programming) {
+      if (!robot.is_programming()) {
+        robot.start_programming();
         digitalWrite(LED_BUILTIN, HIGH);
       } else {
         digitalWrite(LED_BUILTIN, LOW);
-        replay_moves(directions);
-        free_node(directions);
+        robot.end_programming();
       }
     } else {
       struct item<unsigned char, unsigned long>* item = find_item_by_value(codes, results.value);
-
       if (item != NULL) {
-        if (programming) {
-          if (directions == NULL) {
-            directions = create_node(item->key);
-          } else {
-            add_node(directions, item->key);
-          }
-        } else {
-          execute(item->key);
-        }
+        robot.execute(item->key);
       }
     }
   }
   delay(ROBOT_DELAY_LOOP);
+}
+
+/****************************************
+ *                                      *
+ * Robot logic                          *
+ *                                      *
+ ****************************************/
+
+bool Robot::is_programming() {
+  return programming;
+}
+
+void Robot::start_programming() {
+  programming = 1;
+}
+
+void Robot::end_programming() {
+  programming = 0;
+  replay_moves();
+}
+
+void Robot::move(unsigned char forward) {
+  if (forward) {
+    Serial.println("Moving forward");
+  } else {
+    Serial.println("Moving reverse");
+  }
+  digitalWrite(ROBOT_RIGHT_MOTOR_PIN1, forward);
+  digitalWrite(ROBOT_RIGHT_MOTOR_PIN2, !forward);
+
+  digitalWrite(ROBOT_LEFT_MOTOR_PIN1, forward);
+  digitalWrite(ROBOT_LEFT_MOTOR_PIN2, !forward);
+
+  delay(ROBOT_MOVE_LENGHT);  
+}
+
+void Robot::stop() {
+  digitalWrite(ROBOT_RIGHT_MOTOR_PIN1, LOW);
+  digitalWrite(ROBOT_RIGHT_MOTOR_PIN2, LOW);
+
+  digitalWrite(ROBOT_LEFT_MOTOR_PIN1, LOW);
+  digitalWrite(ROBOT_LEFT_MOTOR_PIN2, LOW);
+
+  delay(ROBOT_DELAY_LENGHT);
+}
+
+void Robot::turn(short deg) {
+  Serial.print("Turning by ");
+  Serial.println(deg);
+
+  unsigned char left = LOW;
+  unsigned char right = LOW;
+
+  // turning left:
+  // left motor reverses while right one moves forward
+  if (deg < 0) {
+    right = HIGH;
+    deg = -deg;
+  } else {
+    left = HIGH;
+  }
+
+  unsigned short delayMs = (deg / 90.0) * ROBOT_TURN_LENGHT;
+
+  Serial.print("Move will take ");
+  Serial.print(delayMs);
+  Serial.println(" ms");
+  
+  digitalWrite(ROBOT_LEFT_MOTOR_PIN1, left);
+  digitalWrite(ROBOT_LEFT_MOTOR_PIN2, !left);
+
+  digitalWrite(ROBOT_RIGHT_MOTOR_PIN1, right);
+  digitalWrite(ROBOT_RIGHT_MOTOR_PIN2, !right);
+
+  delay(delayMs);
+}
+
+
+void Robot::execute(unsigned char direction) {
+
+  if (programming) {
+    if (moves == NULL) {
+        moves = create_node(direction);
+    } else {
+        add_node(moves, direction);
+    }
+    return;
+  }
+
+  switch (direction) {
+    case ROBOT_N:
+      move(HIGH);
+      break;
+    case ROBOT_S:
+      move(LOW);
+      break;
+    case ROBOT_W:
+      turn(-90);
+      break;
+    case ROBOT_NW:
+      turn(-45);
+      break;
+    case ROBOT_SW:
+      turn(-135);
+      break;
+    case ROBOT_E:
+      turn(90);
+      break;
+    case ROBOT_NE:
+      turn(45);
+      break;
+    case ROBOT_SE:
+      turn(135);
+      break;
+  }
+  stop();
+}
+
+void Robot::replay_moves() {
+  struct node<unsigned char>* tmp = moves;
+  while (tmp != NULL) {
+    execute(tmp->value);
+    tmp = tmp->next;
+  }
+  free_node(moves);
 }
 
--- ir_robot_control/robot.h
+++ ir_robot_control/robot.h
@@ -1,3 +1,8 @@
+#ifndef robot_h
+#define robot_h
+
+#include "list.h"
+
 /****************************************
  *                                      *
  * Constants                            *
@@ -25,81 +30,12 @@
 // special direction
 #define ROBOT_NO_MOVE 0
 
-/****************************************
- *                                      *
- * IR Remotes                           *
- *                                      *
- * Based on type different codes used   *
- *                                      *
- ****************************************/
-
-// Sony Bravia TV 
-// note: my Sony Bravia - may differ for other models
-#define ROBOT_REMOTE_SONY 1
-// circuits.io simulator
-#define ROBOT_REMOTE_CIRCUITS_IO 2
-// Philips Mini Tower 
-// note: my Philips Mini Tower - may differ for other models
-#define ROBOT_REMOTE_PHILIPS 3
-
-/****************************************
- *                                      *
- * Select the remote type               *
- *                                      *
- ****************************************/
-
-#define ROBOT_REMOTE_SELECTED ROBOT_REMOTE_SONY
-
-#if defined ROBOT_REMOTE_SELECTED && ROBOT_REMOTE_SELECTED == ROBOT_REMOTE_SONY
-
-#define ROBOT_IR_REMOTE_1 0x207BEF0F
-#define ROBOT_IR_REMOTE_2 0xE8455D8E
-#define ROBOT_IR_REMOTE_3 0xCBB7E949
-#define ROBOT_IR_REMOTE_4 0x2C1F3172
-#define ROBOT_IR_REMOTE_6 0x8CE1E3FC
-#define ROBOT_IR_REMOTE_7 0x315B1905
-#define ROBOT_IR_REMOTE_8 0xC67C01B6
-#define ROBOT_IR_REMOTE_9 0xB418D969
-#define ROBOT_IR_REMOTE_FUNC 0x2B4DA162
-
-#endif
-
-#if defined ROBOT_REMOTE_SELECTED && ROBOT_REMOTE_SELECTED == ROBOT_REMOTE_PHILIPS
-
-#define ROBOT_IR_REMOTE_1 0xC54DA1E5
-#define ROBOT_IR_REMOTE_2 0xDD54FA64
-#define ROBOT_IR_REMOTE_3 0x52394D26
-#define ROBOT_IR_REMOTE_4 0x90A84C9A
-#define ROBOT_IR_REMOTE_6 0xBBB5D0DB
-#define ROBOT_IR_REMOTE_7 0x898E1742
-#define ROBOT_IR_REMOTE_8 0xAFBC74A
-#define ROBOT_IR_REMOTE_9 0xBAEBCA37
-#define ROBOT_IR_REMOTE_FUNC 0xA26697EF
-
-#endif
-
-#if defined ROBOT_REMOTE_SELECTED && ROBOT_REMOTE_SELECTED == ROBOT_REMOTE_CIRCUITS_IO
-
-#define ROBOT_IR_REMOTE_1 0xFD08F7
-#define ROBOT_IR_REMOTE_2 0xFD8877
-#define ROBOT_IR_REMOTE_3 0xFD48B7
-#define ROBOT_IR_REMOTE_4 0xFD28D7
-#define ROBOT_IR_REMOTE_6 0xFD6897
-#define ROBOT_IR_REMOTE_7 0xFD18E7
-#define ROBOT_IR_REMOTE_8 0xFD9867
-#define ROBOT_IR_REMOTE_9 0xFD58A7
-#define ROBOT_IR_REMOTE_FUNC 0XFD40BF
-
-#endif
-
 // motor 1 pins
 #define ROBOT_RIGHT_MOTOR_PIN1 10
 #define ROBOT_RIGHT_MOTOR_PIN2 9
 // motor 2 pins
 #define ROBOT_LEFT_MOTOR_PIN1 5
 #define ROBOT_LEFT_MOTOR_PIN2 3
-// IR pin
-#define ROBOT_IR_PIN 11
 
 // move lenght in ms
 #define ROBOT_MOVE_LENGHT 4000
@@ -110,11 +46,22 @@
 // delay in the loop function
 #define ROBOT_DELAY_LOOP 1000
 
+class Robot {
+  public:
+    bool is_programming();
+    void start_programming();
+    void end_programming();
+    void execute(unsigned char direction);
 
-void move(unsigned char forward);
+  private:
+    bool programming;
+    struct node<unsigned char>* moves;
+    void move(unsigned char forward);
+    void stop();
+    void turn(short deg);
+    void replay_moves();
+};
 
-void stop();
 
-void turn(short deg);
+#endif
 
-void execute(unsigned char direction);
